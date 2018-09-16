15681df731de06dfc2f776932675324bf079d9b6
wflixu <wflixu@yeah.net>
2018-09-16 10:42:48 +0800

    优化算法吗，添加测试用例

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const db = require('./data.js');
 function addCode(arr) {
-//   var arr = ['陕西省', '西安市', '雁塔区'];
+  //   var arr = ['陕西省', '西安市', '雁塔区'];
   const res = {
     province: arr[0],
     province_id: -1,
@@ -10,27 +9,45 @@ function addCode(arr) {
     district_id: -1,
     address: ''
   };
-   
+  const db = require('./data.js');
+  let curCollection = [];
+
+  //查找省代码
   for (let i = 0; i < db.length; i++) {
     // console.log(db[i].name)
     if (arr[0] == db[i].name) {
       res.province_id = db[i].code + '0000';
-    }
-    for (let j = 0; j < db[i].children.length; j++) {
-        // console.log(db[i].children[j].name)
-      if (arr[1] == db[i].children[j].name) {
-        res.city_id = db[i].children[j].code + '00';
-      }
-      for (let k = 0; k < db[i].children[j].children.length; k++) {
-        // console.log(db[i].children[j].children[k].name);
-        if (arr[2] == db[i].children[j].children[k].name) {
-            res.district_id = db[i].children[j].children[k].code;
-            break;
-          }
-      }
+      curCollection = db[i].children;
+      break;
     }
   }
-  res.address = arr[0]+arr[1]+arr[2];
+
+ //直辖市判断
+ if(arr[0]==arr[1]){
+  res.city_id = curCollection[0].code + '00';
+  curCollection = curCollection[0].children;
+ } else{
+   //查找市
+  for (let j = 0; j < curCollection.length; j++) {
+    if (arr[1] == curCollection[j].name) {
+      res.city_id = curCollection[j].code + '00';
+      curCollection = curCollection[j].children;
+      break;
+    }
+  }
+ }
+  
+
+  //查找区县
+  for (let k = 0; k < curCollection.length; k++) {
+    if (arr[2] == curCollection[k].name) {
+      res.district_id = curCollection[k].code;
+      break;
+    }
+  }
+  res.address += arr[0];
+  res.address += arr[0]==arr[1]?'':arr[1];
+  res.address += arr[2];
   return res;
 
   //
--- test.js
+++ test.js
@@ -1,8 +1,24 @@
 
-const utils = require('wx-picker-region-addcode');
+// const utils = require('wx-picker-region-addcode');
+const utils = require('./index.js');
+
 console.log("测试开始\n");
 console.log('输入')
-const mock =  ['陕西省', '西安市', '雁塔区'];
+const mock =  ['北京市', '北京市', '东城区'];
 console.log(mock)
 console.log('结果')
 console.log(utils.addCode(mock));
+
+
+console.log('*****************************************************************')
+const mock1 =  ['天津市', '天津市', '蓟州区'];
+console.log(mock1)
+console.log('结果')
+console.log(utils.addCode(mock1));
+
+
+console.log('*****************************************************************')
+const mock2 =  ['新疆维吾尔自治区', '自治区直辖县级行政区划', '阿拉尔市'];
+console.log(mock2)
+console.log('结果')
+console.log(utils.addCode(mock2));
