d10a78de29ac9c1469a20b08550e388cf291daaa
wflixu <wflixu@yeah.net>
2020-03-18 23:55:38 +0800

    test case

--- test/test.js
+++ test/test.js
@@ -5,20 +5,22 @@ console.log("输入");
 const mock = ["北京市", "北京市", "东城区"];
 console.log(mock);
 console.log("结果");
-console.log(utils.addCode(mock));
+console.log(addCode(mock));
 
 console.log(
   "*****************************************************************"
 );
 const mock1 = ["天津市", "天津市", "蓟州区"];
+console.log("输入");
 console.log(mock1);
 console.log("结果");
-console.log(utils.addCode(mock1));
+console.log(addCode(mock1));
 
 console.log(
   "*****************************************************************"
 );
 const mock2 = ["新疆维吾尔自治区", "自治区直辖县级行政区划", "阿拉尔市"];
+console.log("输入");
 console.log(mock2);
 console.log("结果");
-console.log(utils.addCode(mock2));
+console.log(addCode(mock2));
